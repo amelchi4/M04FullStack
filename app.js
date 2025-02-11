021c5e9ba9024499e19ad26952764428629c08d1
amelchi4 <150303318+amelchi4@users.noreply.github.com>
2025-02-10 21:50:25 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,32 @@
+//setup
+const express=require("express")
+var cors = require('cors')
+//activation
+const app=express()
+app.use(cors())
+const router=express.Router()
+//starting web server
+
+//making an api using routes
+router.get("/songs", function(req,res){
+    const songs = [
+        {
+            title: "We Found Love",
+            artist: "Rihanna",
+            popularity: "10",
+            releaseDate: new Date(2011,9,22),
+            genre: ["electro house"]
+        },
+        {
+            title: "Happy",
+            arist: "Pharell Williams",
+            popularity: "10",
+            releaseDate: new Date(2013,11,21),
+            genre:["soul", "new soul"]
+        }
+    ];
+    res.json(songs)
+});
+
+app.use("/api", router)
+app.listen(3000)
